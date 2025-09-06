67bed1d9cdbfb8d2c6c8a7a247a934d884af99c1
Сайко Егор Сергеевич <SaykoES@dom201.local>
2025-09-06 12:08:10 +0700

    Fixed scaling

--- WpfAppCollections/WpfAppCollections/MainWindow.xaml.cs
+++ WpfAppCollections/WpfAppCollections/MainWindow.xaml.cs
@@ -24,7 +24,15 @@ namespace WpfAppCollections
     
     public partial class MainWindow : Window
     {
-        private List<UserControl1> _controlSrc = new List<UserControl1>();
+        private List<UserControl1> _controlSrc = new List<UserControl1>()
+        {
+            new UserControl1() { productImgPath = "https://upload.wikimedia.org/wikipedia/en/3/30/CodeGeassmovieposter.png", productName = "Code Geass the Complete Series (Blu-ray)", productDesc = "Code Geass the Complete Series contains anime episodes 1-25 of Season 1...", productManu = "SUNRISE", productPric = 4100, productInStock = 5 },
+            new UserControl1() { productImgPath = "https://i.ebayimg.com/images/g/M9oAAOSwFiRnPPA0/s-l1600.jpg", productName = "Mobile Suit Gundam Wing (Blu-Ray)", productDesc = "The experimental RX-78 Gundam mobile suit is forced into combat with a civilian...", productManu = "SUNRISE", productPric = 7200, productInStock = 5 },
+            new UserControl1() { productImgPath = "https://upload.wikimedia.org/wikipedia/ru/8/82/Re_Zero_volume_1_cover.jpg", productName = "Re:Zero kara Hajimeta Isekai Seikatsu", productDesc = "Subaru Natsuki was just trying to get to the convenience store ... ", productManu = "Yen On", productPric = 999, productInStock = 7 },
+            new UserControl1() { productImgPath = "https://m.media-amazon.com/images/I/81f6kZNIJBL._SL1500_.jpg", productName = "Re:ZERO, Vol. 2 - light novel", productDesc = "Breaking free of his death loop in the royal city, Subaru awakes in an opulent mansion ... ", productManu = "Yen On", productPric = 999, productInStock = 5 },
+            new UserControl1() { productImgPath = "https://m.media-amazon.com/images/I/51To+WasYPL.jpg", productName = "Re:ZERO, Vol. 3 - light novel", productDesc = "Start from zero all over again with these five extra stories ... ", productManu = "Yen On", productPric = 999, productInStock = 6 },
+            new UserControl1() { productImgPath = "https://upload.wikimedia.org/wikipedia/en/c/c3/Danganronpacharacters4.jpg", productName = "Danganronpa: The Animation (Blu-ray)", productDesc = "Hope’s Peak High School is an exclusive private school that only accepts the best of the best...", productManu = "Lerche", productPric = 7100, productInStock = 3 }
+        };
         private List<UserControl1> Controls1 = new List<UserControl1>();
 
         public List<string> manuList = new List<string>();
@@ -34,14 +42,6 @@ namespace WpfAppCollections
         public MainWindow()
         {
             InitializeComponent();
-            _controlSrc.Add(new UserControl1() { productImgPath = "https://upload.wikimedia.org/wikipedia/en/3/30/CodeGeassmovieposter.png", productName = "Code Geass the Complete Series (Blu-ray)", productDesc = "Code Geass the Complete Series contains anime episodes 1-25 of Season 1...", productManu = "SUNRISE", productPric = 4100, productInStock = 5 });
-            _controlSrc.Add(new UserControl1() { productImgPath = "https://i.ebayimg.com/images/g/M9oAAOSwFiRnPPA0/s-l1600.jpg", productName = "Mobile Suit Gundam Wing (Blu-Ray)", productDesc = "The experimental RX-78 Gundam mobile suit is forced into combat with a civilian...", productManu = "SUNRISE", productPric = 7200, productInStock = 5 });
-            _controlSrc.Add(new UserControl1() { productImgPath = "https://upload.wikimedia.org/wikipedia/ru/8/82/Re_Zero_volume_1_cover.jpg", productName = "Re:Zero kara Hajimeta Isekai Seikatsu", productDesc = "Subaru Natsuki was just trying to get to the convenience store ... ", productManu = "Yen On", productPric = 999, productInStock = 7 });
-            _controlSrc.Add(new UserControl1() { productImgPath = "https://m.media-amazon.com/images/I/81f6kZNIJBL._SL1500_.jpg", productName = "Re:ZERO, Vol. 2 - light novel", productDesc = "Breaking free of his death loop in the royal city, Subaru awakes in an opulent mansion ... ", productManu = "Yen On", productPric = 999, productInStock = 5 });
-            _controlSrc.Add(new UserControl1() { productImgPath = "https://m.media-amazon.com/images/I/51To+WasYPL.jpg", productName = "Re:ZERO, Vol. 3 - light novel", productDesc = "Start from zero all over again with these five extra stories ... ", productManu = "Yen On", productPric = 999, productInStock = 6 });
-            _controlSrc.Add(new UserControl1() { productImgPath = "https://upload.wikimedia.org/wikipedia/en/c/c3/Danganronpacharacters4.jpg", productName = "Danganronpa: The Animation (Blu-ray)", productDesc = "Hope’s Peak High School is an exclusive private school that only accepts the best of the best...", productManu = "Lerche", productPric = 7100, productInStock = 3 });
-
-
             Controls1 = _controlSrc;
 
             manuList.Add("All Manufacturers");
